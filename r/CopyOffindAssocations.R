7b55024c75ccd2fb54faf8baebdeb7e730a529f5
Ram Ganesan Athreya <ramgnsn5@gmail.com>
2014-10-30 07:27:51 +0530

    Added Visualizations for ARules & pre ARules Sequences Commit

--- r/CopyOffindAssocations.R
+++ r/CopyOffindAssocations.R
@@ -1,6 +1,4 @@
 #https://en.wikibooks.org/wiki/Data_Mining_Algorithms_In_R/Frequent_Pattern_Mining/The_Apriori_Algorithm
-#http://www.rdatamining.com/examples/association-rules
-#http://cran.r-project.org/web/packages/arules/arules.pdf
 
 #Clear all variables in global environment
 rm(list = setdiff(ls(), lsf.str()));
@@ -8,9 +6,6 @@ setwd('~/Sites/Mobile-CRM/r');
 
 library("arules");
 library("reshape");
-library("arulesViz");
-library("arulesSequences");
-library('arulesNBMiner');
 
 source("mysql.R");
 #dbSendQuery(db, "truncate table tbl_sold_rules");
@@ -29,18 +24,19 @@ for (i in names(sold_data)) {
 #Getting base rules
 transactions <- as(sold_data, "transactions");
 
-#ptm <- proc.time()
+ptm <- proc.time()
 rules <- apriori(transactions, 
-                 parameter=list(support=0.00001, confidence=0.05, target="rules"),
-                 appearance=list(lhs=c("1=0", "2=0", "3=0", "4=0", "5=0"),
-                                 default="both")
+                 parameter=list(support=0.00000001, confidence=0.05)#,
+                 #appearance=list(lhs=c("1=1","2=1","3=1","4=1","5=1"),default="lhs")
                  );
-#print(proc.time() - ptm);
+print(proc.time() - ptm);
 
-#inspect(rules)
+#rules <- eclat(transactions, parameter=list(support=0.00000001));
+#rules <- ruleInduction(rules, transactions, control=list(verbose=TRUE));
+
+#inspect(rules);
 
 #Generating filtering conditions
-
 cols = colnames(sold_data);
 cond1 = NULL;
 cond2 = NULL;
@@ -51,28 +47,19 @@ for(i in 1:length(sold_data)){
 }
 
 #Getting final results
-rules_subset <- subset(rules, 
+rules_subset <- sort(
+  subset(rules, 
+         #(lhs %in% cond1) &
            !(lhs %in% cond2) &
            (rhs %in% cond1)
+  ),
+  decreasing = TRUE,
+  by = "support"
 );
 
-rules <- rules_subset;
-
-#Supp vs Conf Graph
-#plot(rules, control=list(main='Support vs Confidence'));
-
-#Relationship Graph
-plot(rules, method="graph", control=list(type="items", main='Relationship Matrix'))
-
-#Paracoord Graph
-#plot(rules, method="paracoord", control=list(reorder=TRUE, main='Parallel Coordinates'))
-
-rules_subset <- sort(rules_subset, 
-                     decreasing = TRUE,
-                     by = "support");
+#inspect(rules_subset);
 
 rules_subset <- as(rules_subset, "data.frame");
-
 rownames(rules_subset) <- 1:nrow(rules_subset);
 rules_subset <- as(rules_subset, "matrix");
 
@@ -86,6 +73,10 @@ y <- mapply(gsub, "=1", "", y);
 y <- colsplit(y, split=" => ", c("lhs", "rhs"));
 
 rules_subset <- cbind(y, rules_subset[, -1]);
+
+library(arulesViz);
+#plot(rules_subset, method="graph", control=list(type="items"))
+plot(rules.pruned);
 
 #Writing to database
 #dbWriteTable(db, "tbl_sold_rules", rules_subset, append = T, row.names = F);
